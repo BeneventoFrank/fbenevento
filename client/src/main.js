5606b25c2115a0571a07edce26c57613f79a3792
frankbenevento <fb.mentoslabs@gmail.com>
2018-07-30 16:52:29 -0700

    tryin to fix the build

--- client/src/main.js
+++ client/src/main.js
@@ -5,11 +5,8 @@ import "babel-polyfill";
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
-import App from "./App";
+import App from "./app";
 import router from "./router";
-
-
-
 
 new Vue(
     {
--- client/src/views/home/home.vue
+++ client/src/views/home/home.vue
@@ -8,7 +8,6 @@
 
 <script>
   export default {
-    props: ['timeEntries','navDate'],
     data () {
         return { 
         message:'test test test'
