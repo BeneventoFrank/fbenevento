18979313eb60ceaeb8689addd3931f4b44fe8bdd
frankbenevento <fb.mentoslabs@gmail.com>
2018-07-30 16:11:38 -0700

    initial check in with Vue site

---
+++ client/src/main.js
@@ -0,0 +1,25 @@
+// polyfills for IE
+import "es6-promise/auto";
+import "babel-polyfill";
+
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+
+
+
+
+new Vue(
+    {
+    el: "#app",
+    router,
+    template: "<App/>",
+    components: {
+      App
+    },data(){
+        return{
+            message:'frank'
+        }}
+  });
---
+++ client/src/views/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        {{message}}
+    </div>
+</template>
+
+
+
+<script>
+  export default {
+    props: ['timeEntries','navDate'],
+    data () {
+        return { 
+        message:'test test test'
+        }
+    }
+  }
+ </script>
