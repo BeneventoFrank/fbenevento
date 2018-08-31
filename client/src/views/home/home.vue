98d5e3bc1481d4b37fa737db0569bee49a52e57d
frankbenevento <fb.mentoslabs@gmail.com>
2018-08-31 14:56:10 -0700

    fixed the containers. everything is centering nicely

--- client/src/views/home/home.vue
+++ client/src/views/home/home.vue
@@ -1,37 +1,33 @@
 
 <template>
     <div>
-
-    <section id='mainHeader' class='mainHeader'>
-        <nav class='flexContainer mainNav'>
-            <a class='js-scroll-trigger' href="#homeImage">Home</a>
-            <a class='js-scroll-trigger' href="#aboutMe">About</a>
-            <a class='js-scroll-trigger' href="#resume">Resume</a>
-            <a class='js-scroll-trigger' href="#contactMe">Contact</a>
-        </nav>
-    </section>
-
-    <div id='container' class='flexContainer stackAndCenter '>
-
-        <section id='homeImage' class='homeImage'>
-            <header class="masthead">
-                <div class="flexContainer container fillViewPort centerIntro">
-                    <div class="intro-text">
-                        <div class="intro-lead-in">Welcome To My Site!</div>
-                    </div>
-                </div>
-            </header>
+        <section id='mainHeader' class='mainHeader'>
+            <nav class='flexContainer mainNav'>
+                <a class='js-scroll-trigger' href="#homeImage">Home</a>
+                <a class='js-scroll-trigger' href="#aboutMe">About</a>
+                <a class='js-scroll-trigger' href="#resume">Resume</a>
+                <a class='js-scroll-trigger' href="#contactMe">Contact</a>
+            </nav>
         </section>
-        <section id="mainContent" class="mainContentContainer">
-            <div class='flexContainer sectionContainer'>
+        <div id='container' class='flexContainer stackAndCenter '>
+            <section id='homeImage' class='homeImage'>
+                <header class="masthead">
+                    <div class="flexContainer container fillViewPort centerIntro">
+                        <div class="intro-text">
+                            <div class="intro-lead-in">Welcome To My Site!</div>
+                        </div>
+                    </div>
+                </header>
+            </section>
+            <section id="mainContent" class="flexContainer mainContentContainer">
                 <div id='aboutMe' class='vertLine'>&nbsp;</div>                
                 <div class='sectionHeading'>
                     <p class='sectionTitle'>About Me</p>
                 </div>
                 <div class='vertLine'>&nbsp;</div>
                 <div class='horizontalRight'>&nbsp;</div>
-
                 <section class='lineRight'>
+                    <br>
                     <section class='reduceForLines aboutMe'>
                         <div class='mainSectionContent'>
                             <section class="flexContainer">
@@ -53,8 +49,8 @@
                             </section>
                         </div>
                     </section>
+                    <br>
                 </section>
-
                 <div class='horizontalRight'>&nbsp;</div>            
                 <div id='resume' class='vertLine'>&nbsp;</div>            
                 <div class='sectionHeading'>
@@ -62,13 +58,12 @@
                 </div>
                 <div class='vertLine'>&nbsp;</div>
                 <div class='horizontalLeft'>&nbsp;</div>
-
+                
                 <section class='lineLeft'>
                     <div class='mainSectionContent'>                      
+                        <br>
                         <section class='reduceForLines resume'>
                             <div class='flexContainer'>
-
-
                                 <div class="flexContainer contactInfo stackAndCenter">
                                     <img class="card-img-top" src="/static/img/frank.png" alt="Me at my computer">
                                     <p class='name'>Frank Benevento</p>
@@ -79,7 +74,6 @@
                                         <a href="#"><i class="fa fa-linkedin"></i></a>
                                         <a href="#"><i class="fa fa-github"></i></a>                                         
                                     </p>
-                                    
                                 </div>
                                 <div class="resumeContent">
                                     <p class='objectiveText'>Detail oriented software developer with 17+ years of experience designing and developing custom business solutions. I understand the demands of an IT shop at all levels and I am ready to contribute to the team. I believe in ownership of tasks and taking pride in your solutions.</p>                                 
@@ -89,7 +83,7 @@
                                     </section>
                                     <section id="jobExperience">
                                         <h1 class='header sidebar'>Experience</h1>
-                                        <section id='job1' class='job'>
+                                        <section class='job'>
                                             <div class='flexContainer jobHeader'>
                                                 <p class='header'>Senior Software Developer</p>
                                                 <p class='subHeader'>Mortgage Specialists International</p>
@@ -139,7 +133,7 @@
                                             </div> 
 
                                         </section>
-                                        <section id='job2' class='job'>
+                                        <section class='job'>
                                             <div class='flexContainer jobHeader'>
                                                 <p class='header'>Senior Software Developer</p>
                                                 <p class='subHeader'>Mortgage Specialists International</p>
@@ -187,62 +181,12 @@
                                                     <li>Primary system administrator for on-prem hosted Jira ticketing system. Created and maintained all workflows to support the needs of the company.</li>
                                                 </ul>                                            
                                             </div> 
-
-                                        </section>     
-                                        <section id='job3' class='job'>
-                                            <div class='flexContainer jobHeader'>
-                                                <p class='header'>Senior Software Developer</p>
-                                                <p class='subHeader'>Mortgage Specialists International</p>
-                                            </div>
-                                            <div class='flexContainer jobHeader'>
-                                                <p class='subHeader'>Sept 2011 - June 2018 (6yrs 10mos)</p>
-                                                <p class='subHeader'>Hurst, Texas</p>
-                                            </div>
-                                            <div class='flexContainer'>
-                                                <div>
-                                                    <p class='subHeader medWidth'>Technology Stack:</p>
-                                                </div>
-                                                <div class='flexContainer techCallout mg-l'>
-                                                    <p class='subHeader unl'>Day to Day</p>
-                                                    <br>
-                                                    <ul>
-                                                        <li>Node.js</li>
-                                                        <li>Javascript</li>
-                                                        <li>Web API</li>
-                                                        <li>HTML/CSS/FlexBox</li>
-                                                        <li>SQL</li>
-                                                    </ul>
-                                                </div>
-                                                <div class='mg-l'>
-                                                    <p class='subHeader unl'>Familar With</p>
-                                                    <br>
-                                                    <ul>
-                                                        <li>Vue.js</li>
-                                                        <li>MVC</li>
-                                                        <li>MySQL</li>
-                                                        <li>C#</li>
-                                                        <li>Feathers.js</li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                            <p class='subHeader medWidth'>Experience:</p>                            
-                                            <div class='mg-l'>
-                                                <ul>
-                                                    <li>Created feature rich custom application modules to meet the needs of increasing business. </li>
-                                                    <li>Participated in all large-scale initiatives.  As one of the more senior members of the team, my role was to always provide insight into possible pitfalls that may be overlooked while planning is taking place. </li>
-                                                    <li>At the request of the CEO, relocated to Jacksonville Florida where I assembled a development team that would consume 100% support for 2 of the 4 business lines.    </li>
-                                                    <li>Create and maintain TFS builds for a wide variety of applications.</li>
-                                                    <li>Plan, coordinate and execute production releases using multiple deployment mechanisms  </li>
-                                                    <li>Responsible for debugging and resolving production issues timely and effectively.</li>
-                                                    <li>Primary system administrator for on-prem hosted Jira ticketing system. Created and maintained all workflows to support the needs of the company.</li>
-                                                </ul>                                            
-                                            </div> 
-
-                                        </section>                                                                               
+                                        </section>
                                     </section>                                        
                                 </div>
                             </div>
                         </section>
+                        <br>
                     </div>
                 </section>
                 <div class='horizontalLeft'>&nbsp;</div>
@@ -254,16 +198,16 @@
                 <div class='horizontalRight'>&nbsp;</div>
 
                 <section class='lineRight'>
+                    <br>
                     <section class='reduceForLines contactMe'>
                         <div class='mainSectionContent'>
                                 content goes here
                         </div>
                     </section>
+                    <br>
                 </section>                                            
-
-            </div>        
-        </section>
-    </div>
+            </section>
+        </div>
     </div>
 </template>
 
